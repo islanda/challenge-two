65f8205c139ed76d9ffe6830e389a97e664a9faa
Islanda Naughton <islanda.khau@gmail.com>
2015-10-09 15:49:55 -0400

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-onLoad="self.focus();document.whoo.portfolio-name.focus()"
+/* 
+
+When the user enters a number in the #card input field
+Call jQuery Credit Card Validator plugin
+.validateCreditCard([ callback ] [,options])
+
+	if result.card_type.amex
+		make the opacity of the american express div background 100%
+
+	else if result.card_type.visa
+		make the opacity of the visa div background 100%
+
+	else if result.card_type.discover
+		make the opacity of the discover background div 100%
+
+	else if result.card_type.mastercard
+		make the opacity of the mastercard background div 100%
+*/
+
+$('#card').validateCreditCard(function(result) {
+	alert('CC type: ' + result.card_type.name);
+});
+
+// Take the (result.card_type.name) and .fadeTo ("fast" , 1);

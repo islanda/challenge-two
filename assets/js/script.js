ccbe9e211aeba91c4d73e677099964bf2233b82a
Islanda Naughton <islanda.khau@gmail.com>
2015-10-14 16:52:27 -0400

    added modernizr

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,3 @@
-/* 
-
-When the user enters a number in the #card input field
-Call jQuery Credit Card Validator plugin
-.validateCreditCard([ callback ] [,options])
-
-	if result.card_type.amex
-		make the opacity of the american express div background 100%
-
-	else if result.card_type.visa
-		make the opacity of the visa div background 100%
-
-	else if result.card_type.discover
-		make the opacity of the discover background div 100%
-
-	else if result.card_type.mastercard
-		make the opacity of the mastercard background div 100%
-*/
-
 $('#card').validateCreditCard(function(result) {
 	// alert('CC type: ' + result.card_type.name);
 	$('label[for='+ result.card_type.name +'] input').prop("checked", true);
@@ -26,3 +7,9 @@ $('#card').validateCreditCard(function(result) {
 // $('#card').validateCreditCard(function(result) {
 // 	$('.security div' +  result.card_type.name).css('background-position','no-repeat 0 -81px');
 // });
+
+Modernizr.load({
+	test : Modernizr.input.required,
+	nope : ['assets/js/parsley.min.js']
+		$('#form').parsley();
+});

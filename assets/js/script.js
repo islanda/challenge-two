d5efc6f0545c5dc971a2556ade362f8d73865241
Islanda Naughton <islanda.khau@gmail.com>
2015-10-15 17:46:26 -0400

    ore js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 $('#card').validateCreditCard(function(result) {
 	// alert('CC type: ' + result.card_type.name);
-	$('label[for='+ result.card_type.name +'] input').prop("checked", true);
+		if (result.card_type === null) {
+			$(".credit-cards input").prop("checked", false);
+		} else {
+			$('label[for='+ result.card_type.name +'] input').prop("checked", true);
+		}
 
 // When AMEX is checked or true - 
 	if (result.card_type.name === 'amex') {
